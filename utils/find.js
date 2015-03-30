ee7f197a8f7c5897e1a89fda1e2c29fb09c88a02
Alexander Abashkin <monolithed@gmail.com>
2015-03-30 13:03:46 +0300

    rm try/catch

--- utils/find.js
+++ utils/find.js
@@ -18,36 +18,31 @@ module.exports = function (files, options, callback) {
     return files.map(function (file) {
         var build = {};
 
+        file = path.resolve(options.path, file, 'index');
+
         try {
-            file = path.resolve(options.path, file, 'index');
-
-            try {
-                build.info = require(file + '.json');
-            }
-            catch (error) {
-                throw log.fail('Info file reading', error);
-            }
-
-            try {
-                build.file = resolve.sync(build.info.name);
-            }
-            catch (error) { }
-
-            try {
-                build.file = fs.readFileSync(build.file || file + '.js', 'utf8');
-            }
-            catch (error) {
-                throw log.fail('Source file reading', error);
-            }
-
-            if (callback) {
-                callback(build, file, files);
-            }
-
-            return build;
+            build.info = require(file + '.json');
         }
         catch (error) {
-            throw log.fail('Path reading', error);
+            throw log.fail('Info file reading', error);
         }
+
+        try {
+            build.file = resolve.sync(build.info.name);
+        }
+        catch (error) { }
+
+        try {
+            build.file = fs.readFileSync(build.file || file + '.js', 'utf8');
+        }
+        catch (error) {
+            throw log.fail('Source file reading', error);
+        }
+
+        if (callback) {
+            callback(build, file, files);
+        }
+
+        return build;
     });
 };
