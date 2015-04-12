db1a7ad69f0eecd8ad49632acd1d45297ef8c424
Alexander Abashkin <monolithed@gmail.com>
2015-04-13 02:27:18 +0300

    rm details.wrapper

--- trunk/details/index.js
+++ trunk/details/index.js
@@ -3,6 +3,5 @@
 import feature_info from './feature_info';
 import resolve_path from './resolve_path';
 import dependency_list from './dependency_list';
-import wrapper from './wrapper';
 
-export default { feature_info, resolve_path, dependency_list, wrapper }
+export default { feature_info, resolve_path, dependency_list }
