5442142b60e7b3d046e2a1ec0e21473cda0df67c
Alexander Abashkin <monolithed@gmail.com>
2015-04-13 02:28:58 +0300

    details.dependencies

---
+++ trunk/details/dependencies.js
@@ -0,0 +1,33 @@
+'use strict';
+
+import tsort from 'tsort';
+
+import feature_info from './feature_info';
+import log from '../../utils/log';
+
+/**
+ * An ordered list of requested features
+ *
+ * @param {Array} files
+ * @returns {Array}
+ */
+export default (features) => {
+    let graph = tsort();
+
+    features.forEach((feature) => {
+        let name = feature.name;
+
+        graph.add(name, ...feature_info(name)
+            .dependencies);
+    });
+
+    try {
+        graph = graph.sort();
+    }
+    catch (error) {
+        throw log.error('dependency_list', {
+            text: 'Unresolved dependency error', error });
+    }
+
+    return graph.reverse();
+};
