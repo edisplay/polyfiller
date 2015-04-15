8521a3766c2d27d16d2b32abe6aba5ad96937179
Alexander Abashkin <monolithed@gmail.com>
2015-04-15 03:46:24 +0300

    fixed test

--- tests/find.js
+++ tests/find.js
@@ -4,16 +4,15 @@ import polyfiller from '../index';
 
 describe('find', function () {
     it('test#1', function () {
-        //polyfiller.find([
-        //      {
-        //          name: 'Promise',
-        //          dependencies: true
-        //      }
-        //  ],
-        //  null, (polyfill, name, dependencies) => {
-        //      console.log(name, polyfill.source, polyfill.config);
-        //  });
-        //
+        polyfiller.find([
+            {
+                name: 'Promise',
+            }
+        ],
+        (polyfill, name, dependencies) => {
+            console.log(name, polyfill.source, polyfill.config);
+        });
+
         expect(0).to.equal(1);
     });
 });
