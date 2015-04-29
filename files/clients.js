462a5b0db55a08d9f71fb000de5830e956384101
Alexander Abashkin <monolithed@gmail.com>
2015-04-30 02:21:50 +0300

    - tests

--- files/clients.js
+++ files/clients.js
@@ -7,91 +7,97 @@ module.exports = {
     chrome_win: {
         base       : 'SauceLabs',
         browserName: 'chrome',
-        platform   : 'Windows 8.1',
-        version    : '41.0'
+        platform   : 'Windows 7',
+        version    : '41'
     },
 
+/*
     chrome_osx: {
         base       : 'SauceLabs',
         browserName: 'chrome',
         platform   : 'OS X 10.10',
-        version    : '41.0'
+        version    : '41'
     },
 
     chrome_linux: {
         base       : 'SauceLabs',
         browserName: 'chrome',
         platform   : 'Linux',
-        version    : '41.0'
+        version    : '41'
     },
+*/
 
     safari_osx: {
         base       : 'SauceLabs',
         browserName: 'safari',
         platform   : 'OS X 10.10',
-        version    : '8.0'
+        version    : '8'
     },
 
     firefox_win: {
         base       : 'SauceLabs',
         browserName: 'firefox',
-        platform   : 'Windows 8.1',
-        version    : '37.0'
+        platform   : 'Windows 7',
+        version    : '37'
     },
 
+/*
     firefox_osx: {
         base       : 'SauceLabs',
         browserName: 'firefox',
         platform   : 'OS X 10.10',
-        version    : '32.0'
+        version    : '32'
     },
 
     firefox_linux: {
         base       : 'SauceLabs',
         browserName: 'firefox',
         platform   : 'Linux',
-        version    : '37.0'
+        version    : '37'
     },
+*/
 
     ie_9: {
         base       : 'SauceLabs',
         browserName: 'internet explorer',
         platform   : 'Windows 7',
-        version    : '9.0'
+        version    : '9'
     },
 
     ie_10: {
         base       : 'SauceLabs',
         browserName: 'internet explorer',
         platform   : 'Windows 8',
-        version    : '10.0'
+        version    : '10'
     },
 
     ie_11: {
         base       : 'SauceLabs',
         browserName: 'internet explorer',
         platform   : 'Windows 8.1',
-        version    : '11.0'
+        version    : '11'
     },
 
     iphone: {
         base       : 'SauceLabs',
         browserName: 'iphone',
-        platform   : 'OS X 10.10',
-        version    : '8.2'
+        platform   : 'OS X 10.9',
+        version    : '8.1'
     },
 
+/*
     ipad: {
         base       : 'SauceLabs',
         browserName: 'ipad',
         platform   : 'OS X 10.10',
         version    : '8.2'
     },
+*/
 
     android: {
         base       : 'SauceLabs',
         browserName: 'android',
         platform   : 'Linux',
-        version    : '5.1'
+        version    : '4.2'
     }
 };
--- files/karma.client.js
+++ files/karma.client.js
@@ -3,19 +3,22 @@
 var clients = require('./clients');
 
 module.exports = function (config) {
+    var timeout = 100 * 1000;
+
     config.set({
         browsers : Object.keys(clients),
-        reporters: ['dots', 'saucelabs'],
+        reporters: ['progress', 'saucelabs'],
 
-        browserDisconnectTimeout  : 15 * 1000,
+        browserDisconnectTimeout  : timeout,
+        browserNoActivityTimeout  : timeout,
         browserDisconnectTolerance: 2,
-        browserNoActivityTimeout  : 60 * 1000,
 
         sauceLabs: {
             testName : 'Polyfiller tests',
             username : process.env.SAUCE_USERNAME,
             accessKey: process.env.SAUCE_ACCESS_KEY,
             doctor   : true,
+            recordScreenshots: false,
         },
 
         customLaunchers: clients
