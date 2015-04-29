cb65985c68d6a57d0269224fbfeacb4fc6e5d5a3
Alexander Abashkin <monolithed@gmail.com>
2015-04-29 03:19:11 +0300

    mocha -> dots

--- files/karma.client.js
+++ files/karma.client.js
@@ -4,13 +4,18 @@ var clients = require('./clients');
 
 module.exports = function (config) {
     config.set({
-        browsers  : Object.keys(clients),
-        reporters : ['mocha', 'saucelabs'],
+        browsers : Object.keys(clients),
+        reporters: ['dots', 'saucelabs'],
+
+        browserDisconnectTimeout  : 15 * 1000,
+        browserDisconnectTolerance: 2,
+        browserNoActivityTimeout  : 60 * 1000,
 
         sauceLabs: {
-            testName: 'Polyfiller tests',
-            username:  process.env.SAUCE_USERNAME,
+            testName : 'Polyfiller tests',
+            username : process.env.SAUCE_USERNAME,
             accessKey: process.env.SAUCE_ACCESS_KEY,
+            doctor   : true,
         },
 
         customLaunchers: clients
