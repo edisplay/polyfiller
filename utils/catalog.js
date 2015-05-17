7fa5971c272845c765d72ac23f3d6dd520424fcf
Alexander Abashkin <monolithed@gmail.com>
2015-05-17 20:19:09 +0300

    added links to catalog

--- utils/catalog.js
+++ utils/catalog.js
@@ -2,38 +2,56 @@
 
 import grunt from 'grunt';
 import Polyfiller from '../index';
+import functional from '../tools/functional';
 import deindent from '../tools/deindent';
-
-let polyfiller = new Polyfiller,
-    features = polyfiller.list(true).reverse();
 
 let table = deindent`| Feature | What's included? | Type | License |
                      |:--------|:-----------------|:----:|:-------:|
 `;
 
-let icons = "https://github.com/Polyfiller/polyfiller/" +
-    "blob/master/files/icons/type/25x25";
+let body = {
+    'Feature' (feature) {
+        return `[${feature.name}](${feature.repository.url})`;
+    },
 
-for (let feature of features) {
-    let files = feature.files.map(file => {
-        return `[${file.name}](${file.info})<br />`;
-    });
+    'What\'s included?' (feature) {
+        let files = feature.files.map(file => {
+            return `[${file.name}](${file.info})<br />`;
+        });
 
-    let types = feature.types.map(type => {
-        return deindent `<img src="${icons}/${type}.png?raw=true" \
-            alt="${type}" width="25" height="25" />`;
-    });
+        return files.join('');
+    },
 
-    let licenses = feature.licenses.map(license => {
-        return license.type;
-    });
+    'Type' (feature) {
+        let icons = "https://github.com/Polyfiller/polyfiller/" +
+            "blob/master/files/icons/type/25x25";
 
-    table += deindent`| \
-        ${feature.name} | \
-        ${files.join('')} | \
-        ${types.join(' ')} | \
-        ${licenses.join(', ')} |
-    `;
+        let types = feature.types.map(type => {
+            return deindent `<img src="${icons}/${type}.png?raw=true" \
+                alt="${type}" width="25" height="25" />`;
+        });
+
+        return types.join(' ');
+    },
+
+    'License' (feature) {
+        let licenses = feature.licenses.map(license => {
+            return license.type;
+        });
+
+        return licenses.join(', ');
+    }
+}
+
+let polyfiller = new Polyfiller,
+    features = polyfiller.list(true);
+
+for (let feature of features.reverse()) {
+    for (let [name, value] of functional.entries(body)) {
+        table += `${value(feature)} | `;
+    }
+
+    table += '\n';
 }
 
 grunt.file.write('./files/docs/catalog.md', table);
